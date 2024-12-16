3757bd41578057200f41d38172430147aa5c30f2
VittalVelturi <vittal.velturi@gmail.com>
2024-12-16 23:43:41 +0530

    Change the bucket name in backend file as the name already exists

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-  bucket = "my-terra-bucket"
+  bucket = "my-terra-bucket-vv"
   key = "terraform/state.tf"
   region = "ap-south-1"
   dynamodb_table = "terraform_dynamodb"
